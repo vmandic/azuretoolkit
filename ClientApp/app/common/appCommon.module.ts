0e31d145ed7ea0e1013873c5821b5f93744e8811
Vedran Mandić <mandic.vedran@gmail.com>
2018-07-01 08:50:44 +0200

    added user auth

--- ClientApp/app/common/appCommon.module.ts
+++ ClientApp/app/common/appCommon.module.ts
@@ -1,8 +1,9 @@
+import { UserService } from './services/userService';
 import { NgModule } from '@angular/core';
 import { CognitiveService } from './services/cognitive.service';
 import { AzureHttpClient } from './services/azureHttpClient';
 
 @NgModule({
-    providers: [CognitiveService, AzureHttpClient],
+    providers: [CognitiveService, AzureHttpClient, UserService],
 })
 export class AppCommonModule { }
--- ClientApp/app/components/home/home.component.ts
+++ ClientApp/app/components/home/home.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { User } from './../../common/models/user';
+import { UserService } from './../../common/services/userService';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'home',
     templateUrl: './home.component.html'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+    user: User | null = null;
+
+    ngOnInit(): void {
+        this.userService.getUser().subscribe(user => this.user = user);
+    }
+    constructor(private userService: UserService) {
+
+    }
 }
